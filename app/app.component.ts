f271a0cbfef6ba84eb40800e9eb79a2e2b1f1cfb
Kevin Languasco <kevin.languasco@pucp.pe>
2016-08-10 20:57:42 -0500

    First commit

---
+++ app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+export class Hero {
+    id: number;
+    name: string;
+}
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+    </div>`
+})
+
+export class AppComponent {
+    title = 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
